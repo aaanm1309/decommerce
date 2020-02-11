87553eb637d8f710cd44c6413a0bf86eefcf6119
aaanm1309 <aaanm1309@gmail.com>
2020-02-11 02:37:51 -0300

    1.00.012

--- decommerce/settings.py
+++ decommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'core',
     'catalog',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,3 +138,6 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+
+django_heroku.settings(locals())
